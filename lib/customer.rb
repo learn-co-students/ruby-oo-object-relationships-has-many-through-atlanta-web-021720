edd8700926724a455ac2bdf0034f4eeb0805113a
laarchambault <laarchambault@gmail.com>
2020-02-22 15:11:55 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,47 @@
+# require 'pry'
+
+# require_relative './waiter.rb'
+# require_relative './meal.rb'
+
 class Customer
-  
+        attr_accessor :name, :age, :meal
+        @@all = []
+
+        def initialize(name, age)
+            @name = name
+            @age = age
+            @@all << self
+        end
+
+        def self.all
+            @@all
+        end
+
+        def new_meal(waiter, total, tip=0)
+            Meal.new(waiter, self, total, tip)
+        end
+
+        def meals
+            Meal.all.select { |meal|
+                meal.customer == self
+            }
+        end
+
+        def waiters
+            self.meals.map { |meal|
+                meal.waiter
+            }
+        end
+
 end
+
+# terrance = Customer.new("Terrance", 27)
+# jason = Waiter.new("Jason", 4)
+# andrew = Waiter.new("Andrew", 7)
+# yomi = Waiter.new("Yomi", 10)
+
+# terrance.new_meal(jason, 50, 6)
+# terrance.new_meal(andrew, 60, 8)
+# terrance.new_meal(yomi, 30, 4)
+
+# binding.pry
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,59 @@
+
 class Waiter
 
+    attr_accessor :name, :yrs_experience, :meal
+
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip=0)
+    end
+
+    def meals
+        Meal.all.select { |meal|
+            meal.waiter == self
+        }
+    end
+
+    def best_tipper
+        highest_tip = 0
+        generous_customer = nil
+        meals.each { |meal|
+            if meal.tip > highest_tip
+                highest_tip = meal.tip
+                generous_customer = meal.customer
+            end
+        }
+        generous_customer
+    end
+
+    #code below did not work. error "uninitialized constant Waiter::Meals"
+    # def frequent_customer
+    #     customer_count = {}
+    #     meals.each { |meal|
+    #         customer_count[(meal.customer.name)] ||= 0
+    #         customer_count[(meal.customer.name)] += 1
+    #     }
+    #     highest_frequency = 0
+    #     frequent_customer = nil
+    #     customer_count.select { |customer, count|
+    #         if count > highest_frequency
+    #             highest_frequency = count
+    #             frequent_customer = customer
+    #         end
+    #     }
+    #     frequent_customer
+    # end
+
 end
+
